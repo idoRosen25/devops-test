cb251be95de3102ed832aec219331cf3c621b58e
idoRosen25 <idorosen25@gmail.com>
2023-07-25 20:52:39 +0300

    requires

--- test_guess_num.py
+++ test_guess_num.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
+from webdriver_manager.core.utils import ChromeType
 import time
 
 chrome_options = Options()
